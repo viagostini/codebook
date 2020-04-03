637963826eac68a865e519fd113f3e197f0fa34b
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-04-02 23:40:54 -0300

    refactor and inclusion of upper/lower bound

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,11 @@
 # Iterative Binary Search Function
 
 # returns index of x in arr if present or -1 if not present
-def binarySearch(arr, x):
+def binary_search(arr, x):
     l, r = 0, len(arr)-1
 
     while l <= r:
-        mid = l + (r - l) // 2; 
+        mid = (l + r) // 2; 
 
         # check if x is present at mid 
         if x == arr[mid]: 
@@ -23,25 +23,61 @@ def binarySearch(arr, x):
     return -1
 
 
-# test array 
-arr = [2, 7, 12, 20, 42, 57] 
+# wrapper function for recursive binary search
+def binary_search_recursive (arr, x):
+    return _binary_search_recursive(arr, 0, len(arr)-1, x)
 
-# test case: element present in the beginning
-ans = binarySearch(arr, 2)
-assert ans == 0, "Result was {} but expected result is: 0".format(ans)
+# returns index of x in arr if present or -1 if not present 
+def _binary_search_recursive (arr, l, r, x): 
+	if r >= l: 
+		mid = l + (r - l) // 2
 
-# test case: element present in the middle
-ans = binarySearch(arr, 20)
-assert ans == 3, "Result was {} but expected result is 3".format(ans)
+		# check if x is present at mid
+		if x == arr[mid]: 
+			return mid 
+		
+		# if x is greater, search in right half
+		elif x > arr[mid]: 
+			return _binary_search_recursive(arr, mid+1, r, x) 
+		
+		# if x is smaller, search in left half
+		else:
+			return _binary_search_recursive(arr, l, mid-1, x) 
 
-# test case: element present in the end
-ans = binarySearch(arr, 57)
-assert ans == 5, "Result was {} but expected result is 5".format(ans)
+	else: 
+		# l > r, so x is not present in arr
+		return -1
 
-# test case: element not present and smaller than first element
-ans = binarySearch(arr, -3)
-assert ans == -1, "Result was {} but expected result is -1".format(ans)
+# returns index of first element > x or len(arr) if there is none
+def upper_bound(arr, x):
+    l, r = 0, len(arr)
 
-# test case: element not present and greater than last element
-ans = binarySearch(arr, 60)
-assert ans == -1, "Result was {} but expected result is -1".format(ans)
+    while l < r:
+        mid = (l + r) // 2
+
+        # the answer must be in [l, mid]
+        if arr[mid] > x:
+            r = mid
+        
+        # the answer must be in [mid+1, r]
+        else:
+            l = mid + 1
+
+    return l
+
+# returns index of first element >= x or len(arr) if there is none
+def lower_bound(arr, x):
+    l, r = 0, len(arr)
+
+    while l < r:
+        mid = (l + r) // 2
+
+        # the answer must be in [mid+1, r]
+        if arr[mid] < x:
+            l = mid + 1
+        
+        # the answer must be in [l, mid]
+        else:
+            r = mid
+
+    return l
