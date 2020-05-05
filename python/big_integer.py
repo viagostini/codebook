a8c16dbfae6ef1effac5365287206446884f8cf6
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-05-05 17:08:39 -0300

    updated big integer

--- python/big_integer.py
+++ python/big_integer.py
@@ -1,33 +1,67 @@
 """
-Number is represented as array of digits in reverse order
-For example, to represent 167318 = [8, 1, 3, 7, 6, 1]
+Number is represented as an array of digits in reverse order
+appended by positive or minus sign
 
-For now, we handle only positive numbers
+For example, -167318 = [8, 1, 3, 7, 6, 1, '-']
+             +167318 = [8, 1, 3, 7, 6, 1, '+']
 """
 
 # converts string to big integer array format
-def to_number(string):
-    a = []
-    for c in reversed(string):
-        a.append(int(c))
+def to_number(s):
+    a = [int(c) for c in reversed(s[1:])]
+    a.append(s[0])
     return a
 
 # converts big integer to string format
 def to_string(number):
-    a = ''
-    for c in reversed(number):
-        a += str(c)
-    return a
+    return number[-1] + ''.join([str(c) for c in reversed(number[:-1])])
 
-# return c = a + b
+# returns c = a + b, handles sign
 def add(a, b):
-    # force len(a) >= len(b)
+    if a[-1] == b[-1]:
+        c = _add(a[:-1], b[:-1]) + [a[-1]]
+    
+    else:
+        if greater_than(absolute(a), absolute(b)):
+            c = _subtract(a[:-1], b[:-1]) + [a[-1]]
+        else:
+            c = _subtract(b[:-1], a[:-1]) + [b[-1]]
+    
+    # avoids -0 problem
+    if to_string(c) == '-0':
+        c[-1] = '+'
+    
+    return c
+
+# returns abs(a)
+def absolute(a):
+    b = a.copy()
+    b[-1] = '+'
+    return b
+
+# note: -0 != +0
+def equals(a, b):
+    return to_string(a) == to_string(b)
+
+# returns True if a > b, False otherwise
+# note: remember that we can have +0 and -0
+def greater_than(a, b):
+    if a[-1] != b[-1]:
+        return True if a[-1] == '+' else False
+    
+    if len(a) == len(b):
+        return to_string(a) > to_string(b)
+
+    return True if len(a) > len(b) else False
+
+# return c = a + b for a, b >= 0, without sign in array
+def _add(a, b):
+    # force len(a) >= len(b) since a, b >= 0
     if len(a) < len(b):
         a, b = b, a
     
     # pad smaller number with zeroes
-    len_diff = len(a) - len(b)
-    b.extend([0] * len_diff)
+    b.extend([0] * (len(a) - len(b)))
 
     # perform sum
     carry = 0
@@ -44,5 +78,32 @@ def add(a, b):
 
     return c
 
-def subtract(a, b):
-    pass
+def minus(a):
+    b = a.copy()
+    b[-1] = '+' if b[-1] == '-' else '-'
+    return b
+
+# return c = a - b for a >= b
+def _subtract(a, b):
+    if len(a) >= len(b):
+        b.extend([0] * (len(a) - len(b)))
+    else:
+        a.extend([0] * (len(a) - len(b)))
+
+    c = [0] * len(a)
+    for i in range(len(a)):
+        if a[i] < b[i]:
+            a[i] += 10
+            a[i+1] -= 1
+        c[i] = a[i] - b[i]
+    
+    # remove trailing zeroes except possibly one (to represent 0)
+    while c[-1] == 0 and len(c) > 1:
+        del c[-1]
+
+    return c
+
+# return a - b = a + (-b)
+def subtract(a,b):
+    b[-1] = '+' if b[-1] == '-' else '-'
+    return add(a,b)
--- python/tests/11_big_integer_test.py
+++ python/tests/11_big_integer_test.py
@@ -2,15 +2,43 @@ import pytest
 from big_integer import *
 
 def test_convert_to_number():
-    assert to_number("23782") == [2, 8, 7, 3, 2]
+    assert to_number('+23782') == [2, 8, 7, 3, 2, '+']
+    assert to_number('-23782') == [2, 8, 7, 3, 2, '-']
 
 def test_convert_to_string():
-    assert to_string([2, 8, 7, 3, 2]) == "23782"
+    assert to_string([2, 8, 7, 3, 2, '+']) == '+23782'
+    assert to_string([2, 8, 7, 3, 2, '-']) == '-23782'
 
 def test_add():
-    a = [4, 1]
-    b = [2, 4, 9, 2]
-    assert add(a, b) == [6, 5, 9, 2]
-    a = [4, 1, 1, 9]
-    b = [2, 4, 9, 2]
-    assert add(a, b) == [6, 5, 0, 2, 1]
+    # same sign
+    a, b = [4, 1, '+'], [2, 4, 9, 2, '+']
+    c, d = [4, 1, '-'], [2, 4, 9, 2, '-']
+
+    assert add(a, b) == [6, 5, 9, 2, '+']
+    assert add(c, d) == [6, 5, 9, 2, '-']
+
+    # different signs
+    a, b = [5, 3, 2, '+'], [0, 0, 2, '-'] # contains trailing zeroes
+    c, d = [2, 4, 9, 2, '+'], [4, 1, 1, 9, '-']
+
+    assert add(a, b) == [5, 3, '+']
+    assert add(c, d) == [2, 7, 1, 6, '-']
+
+    # -0 test
+    a, b = [2, 4, 9, 2, '+'], [2, 4, 9, 2, '-']
+    assert add(a, b) == [0, '+']
+
+def test_subtract():
+    # same sign
+    a, b = [4, 1, '+'], [2, 4, 9, 2, '+']
+    c, d = [4, 1, '-'], [2, 4, 9, 2, '-']
+
+    assert subtract(a, b) == [8, 2, 9, 2, '-']
+    assert subtract(c, d) == [8, 2, 9, 2, '+']
+
+    # different signs
+    a, b = [5, 3, 2, '-'], [0, 0, 2, '+'] # contains trailing zeroes
+    c, d = [2, 4, 9, 2, '+'], [4, 1, 1, 9, '-']
+
+    assert subtract(a, b) == [5, 3, 4, '-']
+    assert subtract(c, d) == [6, 5, 0, 2, 1, '+']
